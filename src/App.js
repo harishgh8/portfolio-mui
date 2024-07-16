874a57951f2226dcf4cc6146fa4d7cacd70c9005
harish <harishg.hiriyur@gmail.com>
2024-07-16 11:44:10 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Container, Divider } from "@mui/material";
+import { Container } from "@mui/material";
 import { Box } from "@mui/system";
 import About from "./components/About";
 import Contact from "./components/Contact";
